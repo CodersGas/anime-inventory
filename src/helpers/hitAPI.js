e8de9a225118d078805e31656e80a69ca6792523
Ashish Kumar <rudrashishkumar@gmail.com>
2022-01-14 16:30:15 +0530

    Final fix for form submit loading

--- src/helpers/hitAPI.js
+++ src/helpers/hitAPI.js
@@ -4,7 +4,7 @@ export const hitAPI = async (httpMethod, payload, endpoint) => {
   const apiUrl = (process.env.NODE_ENV === "development" ? process.env.REACT_APP_API_LOCAL_URL : process.env.REACT_APP_API_PROD_URL) + endpoint;
   switch (httpMethod) {
     case "POST":
-      axios.post(
+      await axios.post(
         apiUrl,
         payload
       )
